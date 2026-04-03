81ce39b17211578539212ef13329dc236cc86513
Ian McDonald <irm@pdx.edu>
2026-04-03 14:00:00 -0700

    deleted git shell file contents

--- code/git_commands.sh
+++ code/git_commands.sh
@@ -1,7 +1,1 @@
-echo "# portland_cvr_2024" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/ianrmcdonald-pdx/portland_cvr_2024.git
-git push -u origin main
+#memos for git ccommands
